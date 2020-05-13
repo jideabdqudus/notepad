dc4db99d34c296721ef2d590c321edfad500979e
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 15:24:19 +0100

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Fragment } from "react";
 import "./tailwind.generated.css";
-import styles from "./App.module.css";
+import style from "./App.module.css";
 import { Card, Navbar, Header } from "./component";
 
 function App() {
   return (
     <Fragment>
       <div className="App">
-      <div>
+      <div className={style.body}>
         <Navbar />
         <Header />
         <Card />
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+    What to do?
+  </button>
     </div>
   );
 };
