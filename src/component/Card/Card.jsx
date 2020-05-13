b908f963bd5abbfbfdb820e6ee1d10b9047d704f
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 16:06:12 +0100

    Setting up form Inputs

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Card = () => {
+ const [item, setItem] = useState('')
+
+
+ const formSubmitted=(e)=>{
+   e.preventDefault();
+   
+   console.log ({item})
+
+ }
   return (
     <div>
-      <h1>Card</h1>
+    <div>{item}</div>
+      <form onSubmit={formSubmitted}>
+        <input type="name" value={(e)=> setItem(e.target.value)}  placeholder="Your Item"/>
+        <button type="submit" value="Submit">Submit</button>
+      </form>
     </div>
   );
 };
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import style from "./Header.module.css"
 
 const Header = () => {
   return (
     <div>
-    <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-    What to do?
-  </button>
+      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        What to do?
+      </button>
     </div>
   );
 };
