0bb2af9a146ee5b3d642ccf68ac9f40626e906d3
jideabdqudus <jideabdqudus@gmail.com>
2020-05-13 22:11:16 +0100

    Setting up Form

--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -1,19 +1,22 @@
 import React, {useState} from "react";
 
 const Card = () => {
- const [item, setItem] = useState("")
+ const [item, setItem] = useState([]);
 
+ const formChanged=(e)=>{
+  e.preventDefault();
+  setItem(e.target.value)
+}
 
  const formSubmitted=(e)=>{
    e.preventDefault();
-   setItem(e.target.value)
 
  }
   return (
     <div>
     <div>{item}</div>
       <form onSubmit={formSubmitted}>
-        <input type="name" value={item}  placeholder="Your Item"/>
+        <input type="name" onChange={formChanged} placeholder="Your Item"/>
         <button type="submit" value="Submit">Submit</button>
       </form>
     </div>
