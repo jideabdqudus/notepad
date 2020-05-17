e931e10fa7fb09f087a65fe787009d8e5544f4a4
jideabdqudus <jideabdqudus@gmail.com>
2020-05-17 22:45:36 +0100

    Added Dependency BootStrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import "./tailwind.generated.css";
 import style from "./App.module.css";
 import { Card, Navbar, Header } from "./component";
+import { Button } from 'reactstrap';
 
 function App() {
   return (
