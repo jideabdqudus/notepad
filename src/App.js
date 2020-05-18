9aa81383b4344f1025b5c3861b7afbae82c58310
jideabdqudus <jideabdqudus@gmail.com>
2020-05-18 22:44:06 +0100

    Added Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from "react";
 import "./tailwind.generated.css";
-import style from "./App.module.css";
 import { Card, Navbar, Header } from "./component";
 
 
--- src/component/Card/Card.jsx
+++ src/component/Card/Card.jsx
@@ -6,33 +6,49 @@ import {
   ToastBody,
   Row,
   Col,
+  Button,
   ToastHeader,
 } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Card = () => {
   return (
     <div>
       <Col sm="12" md={{ size: 4, offset: 4 }}>
         <Jumbotron>
-          <div>
+          <Container>
             <div>
-              <h1 className="display-3">Schedule</h1>
-              <br />
-              <Row>
-                <Col sm={10}>
-                  <Toast>
-                    <ToastHeader icon="primary">Reactstrap</ToastHeader>
-                    <ToastBody>
-                      This is a toast with a primary icon — check it out!
-                    </ToastBody>
-                  </Toast>
-                </Col>
-                <Col sm={2}>
-                  <h4>Delete</h4>
-                </Col>
-              </Row>
+              <h3 className="display-4">What to do?</h3>
+              <a href="#!" className="lead" size="sm">Delete all Posts</a>
+              <br /><br/>
+              <div>
+                <Row>
+                  <Col sm={10}>
+                    <Toast>
+                      <ToastHeader icon="danger">Important</ToastHeader>
+                      <ToastBody>Scholarship Application!</ToastBody>
+                    </Toast>
+                  </Col>
+                  <Col sm={2}>
+                    <FontAwesomeIcon icon={faTrash} size="lg" />
+                  </Col>
+                </Row>
+                <br />
+                <Row>
+                  <Col sm={10}>
+                    <Toast>
+                      <ToastHeader icon="warning">Important</ToastHeader>
+                      <ToastBody>Submit Info</ToastBody>
+                    </Toast>
+                  </Col>
+                  <Col sm={2}>
+                    <FontAwesomeIcon icon={faTrash} size="lg" />
+                  </Col>
+                </Row>
+              </div>
             </div>
-          </div>
+          </Container>
         </Jumbotron>
       </Col>
     </div>
