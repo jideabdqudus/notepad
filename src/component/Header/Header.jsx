09147a3b7a26f24bca00aff3b30564aa2875b0ce
jideabdqudus <jideabdqudus@gmail.com>
2020-05-18 21:35:59 +0100

    Added Changes

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import cx from "classnames"
+import cx from "classnames";
 import style from "./Header.module.css";
+import { Container, Row, Col, Button } from "reactstrap";
 
 const Header = () => {
   return (
-    <div className={cx(style.container,style.item)}>
-      <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-bold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        What to do?
-      </button>
-    </div>
+    <Container className="themed-container" fluid="md">
+    <Row xs="2">
+    <Col></Col>
+    <Col></Col>
+    <Col></Col>
+    <Col>Column</Col>
+  </Row>  
+    <Row>
+        <Col sm="12" md={{ size: 6, offset: 3 }}>
+        <Button color="primary" size="lg">Large Button</Button>
+        </Col>
+    </Row>
+    </Container>
   );
 };
 
